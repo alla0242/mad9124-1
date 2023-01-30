79d89865eb11cd26e984369112c0dcf010560679
Tim Robillard <robillt@algonquincollege.com>
2023-01-29 19:40:01 -0500

    graphql example

---
+++ week4/graphql/data/classes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = [
+  {
+    id: "1",
+    name: "MAD9124",
+    instructor: "tim",
+    students: ["1", "2", "3", "4"],
+  },
+  { id: "2", name: "MAD9112", instructor: "steve", students: ["1", "2", "3"] },
+  { id: "3", name: "MAD9022", instructor: "akshay", students: ["1", "2", "4"] },
+  { id: "4", name: "MAD9014", instructor: "will", students: ["1", "2", "4"] },
+];
