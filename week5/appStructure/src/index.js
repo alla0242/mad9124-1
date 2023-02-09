b37e62989189b1c601de42298a18da50f3c04e35
Tim Robillard <robillt@algonquincollege.com>
2023-02-09 18:46:44 -0500

    add morgan

--- week5/appStructure/src/index.js
+++ week5/appStructure/src/index.js
@@ -2,15 +2,15 @@
 
 require("dotenv/config");
 const express = require("express");
+const morgan = require("morgan");
 const studentRouter = require("./router/students");
 
 const app = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/students", studentRouter);
-
-console.log('a', process.env.PORT);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
