46428f3f9bfd89e10327673c062a34844ca105ad
Tim Robillard <robillt@algonquincollege.com>
2023-02-09 18:19:19 -0500

    add .env

--- week5/appStructure/src/controllers/students.js
+++ week5/appStructure/src/controllers/students.js
@@ -1,12 +1,5 @@
 const StudentService = require("../services/students");
 
-const students = [
-  { id: "1", name: "tim", grade: "A+" },
-  { id: "2", name: "jad", grade: "A+" },
-  { id: "3", name: "hena", grade: "A+" },
-  { id: "4", name: "eduardo", grade: "A+" },
-];
-
 const getAll = (_, res) => {
   const data = StudentService.getAll();
   res.json({ data });
--- week5/appStructure/src/index.js
+++ week5/appStructure/src/index.js
@@ -1,13 +1,16 @@
 "use strict";
 
+require("dotenv/config");
 const express = require("express");
-const studentRouter = require('./router/students');
+const studentRouter = require("./router/students");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/students", studentRouter);
+
+console.log('a', process.env.PORT);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
