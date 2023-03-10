cecf47ef3e7000adb14ecff9a52abd310f451d85
Tim Robillard <robillt@algonquincollege.com>
2023-03-09 19:55:04 -0500

    part 2

--- week9/mongoExercises/index.js
+++ week9/mongoExercises/index.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require("mongodb");
+
 require("./db");
 const Dog = require("./models/Dog");
 
@@ -43,10 +45,89 @@ const getAll = async () => {
   return dogs;
 };
 
+const findById = (id) => {
+  // return the dog with that
+  return Dog.findById(id);
+};
 
+const findTheBestDogs = async () => {
+  // return dogs with howGood === 10
+  const perfectDogs = await Dog.find({ howGood: 10 });
+  return perfectDogs;
+};
+
+const findByMatchingName = async (name) => {
+  const regexp = new RegExp(name, "i");
+  const dogs = await Dog.find({ name: regexp });
+  return dogs;
+};
+
+const findGreaterThanDOB = async () => {
+  const dogs = await Dog.find({
+    dob: {
+      $gt: new Date("2010-01-01"),
+    },
+  }).sort({ dob: 1 });
+  return dogs;
+};
+
+const getMyPals = async (id) => {
+  // get by id
+  // populate my pals
+  const dog = await Dog.findById(id).populate("pals").select("pals");
+
+  //return just the array of pals
+  return dog.pals;
+};
+
+const updateOne = async (id) => {
+  // update our dog
+  const updatedDog = await Dog.findByIdAndUpdate(
+    id,
+    {
+      name: "updatedName2",
+    },
+    // need to tell mongoose that you want the updated document
+    {
+      returnOriginal: false,
+    }
+  );
+  // return it
+  return updatedDog;
+};
+
+const updateMyPals = async (id) => {
+  const ids = [
+    new ObjectId("640a7163c8392c3400e06627"),
+    new ObjectId("640a3cdf04c9e90fb5a27488"),
+    new ObjectId("640a7163c8392c3400e06624"),
+  ];
+  // update our dog
+  const updatedDog = await Dog.findByIdAndUpdate(
+    id,
+    {
+      pals: ids,
+    },
+    // need to tell mongoose that you want the updated document
+    {
+      returnOriginal: false,
+    }
+  );
+  // return it
+  return updatedDog;
+};
+
+const deleteById = async (id) => {
+  // delete the dog
+  return await Dog.findByIdAndDelete({ _id: id });
+
+  // return the deleted dog
+};
 
 const main = async () => {
-  const res = await getAll();
+  const id = "640a357ab65c1771f3a1b5ef";
+
+  const res = await deleteById(id);
   console.log(res);
 };
 
